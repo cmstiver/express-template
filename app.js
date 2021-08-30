3df774be2f45d35d05d943d60f644d3bdcdd6e4e
Chayse Stiver <chayse.stiver@gmail.com>
2021-08-30 16:56:03 -0700

    template

---
+++ app.js
@@ -0,0 +1,52 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import indexRouter from './routes/index';
+
+dotenv.config();
+
+// Database Init
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// this has to be module.exports
+module.exports = app;
